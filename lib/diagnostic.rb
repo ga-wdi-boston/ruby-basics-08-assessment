e271bbd4081dfcba59387c15ab76b12e8cc46b7b
msyao-github <msyaois@gmail.com>
2016-02-11 09:27:06 -0500

    ruby diagnostic

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -5,28 +5,34 @@ Diagnostic = OpenStruct.new
 #
 # In a Ruby string, write the command you would use to run a script at
 # `lib/example.rb`
+ruby lib/example.rb
 
-Diagnostic.script = 'your response here'
+Diagnostic.script = 'pry'
 
 # Using the REPL
 #
 # In a Ruby string, write the command you would use to enter the REPL we
 # prefer over `irb`
+pry
 
-Diagnostic.repl = 'your response here'
+Diagnostic.repl = 'name = "star wars episode viii"'
 
 # Naming conventions in Ruby
 #
 # In a Ruby string, write what you would write to properly name a variable in
 # Ruby that read in English as "star wars episode viii".
 
-Diagnostic.variable = 'your response here'
+name = "star wars episode viii"
+puts name
+
+Diagnostic.variable =  "character = #{'Jar Jar Binks'}"
 
 # String interpolation in Ruby
 #
 # Use string interpolation to complete the sentence below. Replace "ANSWER" with
 # the correct code.
 
+ "character = #{'Jar Jar Binks'}"
 character = 'Jar Jar Binks'
 
 Diagnostic.interpolation = 'ANSWER is really a Sith Lord.'
@@ -37,8 +43,12 @@ Diagnostic.interpolation = 'ANSWER is really a Sith Lord.'
 # of multiplying by two the result of adding `a` and `b`. Name the method
 # conventionally.
 
-def method_name
+
+def mult_add(a, b)
+  (a+b)*2
 end
+
+puts mult_add(a,b)
 
 Diagnostic.defined_method = # call your method here
 
@@ -49,6 +59,15 @@ Diagnostic.defined_method = # call your method here
 
 # define your method here
 
+def odd(num)
+   if num % 2 == 1
+      return "#{numb} is odd"
+   end
+   if num % 2 == 0
+      return "#{num} is even"
+  end
+end
+
 Diagnostic.predicate = # call your method here
 
 # Numbers in Ruby
@@ -56,12 +75,12 @@ Diagnostic.predicate = # call your method here
 # What is the type of object in Ruby for decimal numbers? Write your answer in
 # a Ruby string.
 
-Diagnostic.decimal = 'your response here'
+Diagnostic.decimal = 'Float'
 
 # What is the type of object in Ruby for integer numbers? Write your answer in
 # a Ruby string.
 
-Diagnostic.integer = 'your response here'
+Diagnostic.integer = 'Fixnum'
 
 # Write an example of a decimal and an integer in Ruby. Replace the Ruby strings
 # in the following array with your examples.
@@ -88,23 +107,26 @@ else
 end
 
 # Predict what value will be returned. Write your response as a Ruby string.
+'The Dark Knight'
 
-Diagnostic.flow_control = 'your response here'
+Diagnostic.flow_control = 'The Dark Knight'
 
 # In a Ruby string, write what keyword you use for "else if" clauses in Ruby.
+elsif
 
-Diagnostic.else_if = 'your response here'
+
+Diagnostic.else_if = 'elsif'
 
 # Looping in Ruby
 #
 # What method can you call on an array in order to iterate over it in Ruby?
 # Write your answer as a symbol.
 
-Diagnostic.array_iteration = :example
+Diagnostic.array_iteration = :example.each do
 
 # Returns in Ruby
 #
 # Does ruby require an explicit return from methods? Answer true or false, and
 # replace nil.
 
-Diagnostic.returns = nil
+Diagnostic.returns = true
