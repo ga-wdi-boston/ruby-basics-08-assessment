c0af76cf1ab32454d2d4b90c82020be2b320a801
acobrelo <acobrelo@gmail.com>
2016-06-09 09:28:39 -0400

    Answered all questions
    Finished diagnostic.rb

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -7,7 +7,7 @@ Diagnostic = OpenStruct.new
 # `lib/example.rb`
 
 
-Diagnostic.script = ruby lib/example.rb
+Diagnostic.script = 'ruby lib/example.rb'
 
 # Using the REPL
 #
@@ -15,14 +15,14 @@ Diagnostic.script = ruby lib/example.rb
 # prefer over `irb`
 
 
-Diagnostic.repl = pry
+Diagnostic.repl = 'pry'
 
 # Naming conventions in Ruby
 #
 # In a Ruby string, write what you would write to properly name a variable in
 # Ruby that read in English as "star wars episode viii".
 
-Diagnostic.variable = star_wars_episode_vii
+Diagnostic.variable = 'star_wars_episode_vii'
 
 # String interpolation in Ruby
 #
@@ -47,12 +47,12 @@ Diagnostic.predicate = is_odd = array.select(&:odd?)
 # What is the type of object in Ruby for decimal numbers? Write your answer in
 # a Ruby string.
 
-Diagnostic.decimal = num.to_f
+Diagnostic.decimal = 'num.to_f'
 
 # What is the type of object in Ruby for integer numbers? Write your answer in
 # a Ruby string.
 
-Diagnostic.integer = num.to_i
+Diagnostic.integer = 'num.to_i'
 
 # Write an example of a decimal and an integer in Ruby. Replace the Ruby strings
 # in the following array with your examples.
@@ -80,18 +80,18 @@ end
 
 # Predict what value will be returned. Write your response as a Ruby string.
 
-Diagnostic.flow_control = 'your response here'
+Diagnostic.flow_control = 'Just your average billionaire'
 
 # In a Ruby string, write what keyword you use for "else if" clauses in Ruby.
 
-Diagnostic.else_if = 'your response here'
+Diagnostic.else_if = 'elsif'
 
 # Returns in Ruby
 #
 # Does ruby require an explicit return from methods? Answer true or false, and
 # replace nil.
 
-Diagnostic.returns = nil
+Diagnostic.returns = false
 
 # Ruby Arrays
 #
@@ -102,13 +102,15 @@ arr = [12, 34, 56, 67]
 # In a Ruby string, write how you would remove the last two elements from this
 # array
 
-Diagnostic.remove_from_array = 'your answer here'
+Diagnostic.remove_from_array = 'arr.pop(2)'
 
 # What would `arr` equal after:
 arr[arr.length + 2] = 99
 # Write your response as a ruby string
 
-Diagnostic.array_add_to = 'your answer here'
+Diagnostic.array_add_to = '[12, 34, 56, 67, nil, nil, 99]'
+# this answer assumed the last two values weren't removed, otherwise it would be
+# '[12, 34, nil, nil, 99]'
 
 # Ruby Hashes
 #
@@ -117,10 +119,10 @@ Diagnostic.array_add_to = 'your answer here'
 # Use shorthand to make this one line long.
 # Write your response as a Ruby string.
 
-Diagnostic.person_hash = 'your answer here'
+Diagnostic.person_hash = 'person = {age: 42, name: Douglas}'
 
 # Set a default return value to your `person` hash that concatenates the value
 # of person[:first_name] and a message about them.
 # Write your response as a Ruby string.
 
-Diagnostic.hash_default = 'your answer here'
+Diagnostic.hash_default = 'person.default = `#{:first_name}+ 'brought his towel'`'
