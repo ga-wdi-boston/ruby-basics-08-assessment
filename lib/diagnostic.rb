a86cbd7d4b2640c7161f9e94492ae3ed3a3b82b3
jbuzzanco <jcbuzzanco@gmail.com>
2016-11-09 09:27:08 -0500

    answ all questions
    answered  all questions to best of my knowledge

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -4,23 +4,25 @@ Diagnostic = OpenStruct.new
 # Running scripts from the command line
 #
 # In a Ruby string, write the command you would use to run a script at
+
 # `lib/example.rb`
 
-Diagnostic.script = 'your response here'
+Diagnostic.script = pry './lib/example.rb'
 
 # Using the REPL
 #
 # In a Ruby string, write the command you would use to enter the REPL we
+
 # prefer over `irb`
 
-Diagnostic.repl = 'your response here'
+Diagnostic.repl = pry
 
 # Naming conventions in Ruby
 #
 # In a Ruby string, write what you would write to properly name a variable in
 # Ruby that read in English as "star wars episode viii".
 
-Diagnostic.variable = 'your response here'
+Diagnostic.variable = "star wars epsode vii"
 
 # String interpolation in Ruby
 #
@@ -29,14 +31,14 @@ Diagnostic.variable = 'your response here'
 
 character = 'Jar Jar Binks'
 
-Diagnostic.interpolation = 'ANSWER is really a Sith Lord.'
+Diagnostic.interpolation = character + 'ANSWER is really a Sith Lord.'
 
 # Numbers in Ruby
 #
 # What is the type of object in Ruby for decimal numbers? Write your answer in
 # a Ruby string.
 
-Diagnostic.decimal = 'your response here'
+Diagnostic.decimal = 'numbers'
 
 # What is the type of object in Ruby for integer numbers? Write your answer in
 # a Ruby string.
@@ -46,14 +48,14 @@ Diagnostic.integer = 'your response here'
 # Write an example of a decimal and an integer in Ruby. Replace the Ruby strings
 # in the following array with your examples.
 
-Diagnostic.numbers = ['decimal', 'integer']
+Diagnostic.numbers = ['0.5', '3']
 
 # Falsiness in Ruby
 #
 # Write all the values that evaluate to "falsy" in Ruby. Write these values in
 # a Ruby array.
 
-Diagnostic.falsy = []
+Diagnostic.falsy = [false, nil]
 
 # Flow control in Ruby
 #
@@ -69,18 +71,19 @@ end
 
 # What value will be returned. Write your response as a Ruby string.
 
-Diagnostic.flow_control = 'your response here'
+Diagnostic.flow_control = 'nothing will be returned as there is
+nothing asking to be returned'
 
 # In a Ruby string, write what keyword you use for "else if" clauses in Ruby.
 
-Diagnostic.else_if = 'your response here'
+Diagnostic.else_if = 'elsif'
 
 # Returns in Ruby
 #
 # Does ruby require an explicit return from methods? Answer true or false, and
 # replace nil.
 
-Diagnostic.returns = nil
+Diagnostic.returns = false
 
 # Ruby Arrays
 #
@@ -91,13 +94,14 @@ arr = [12, 34, 56, 67]
 # In a Ruby string, write how you would remove the last two elements from this
 # array
 
-Diagnostic.remove_from_array = 'your answer here'
+Diagnostic.remove_from_array = 'use delete method to select "56" and "67"'
 
 # Taking the result from the question above, what would `arr` now equal:
 arr[arr.length + 2] = 99
 # Write your response as a ruby string
 
-Diagnostic.array_add_to = 'your answer here'
+Diagnostic.array_add_to = 'It would not add 99 because that index does not exist
+at that length'
 
 # Ruby Hashes
 #
@@ -106,10 +110,15 @@ Diagnostic.array_add_to = 'your answer here'
 # Use shorthand to make this one line long.
 # Write your response as a Ruby string.
 
-Diagnostic.person_hash = 'your answer here'
+Diagnostic.person_hash = 'class Person
+  def initialize(first_name, last_name)
+  @first_name = first_name
+  @last_name = last_name
+  end
+  Person.first_name('leslie').last_name('knope')'
 
 # Set a default return value to your `person` hash that concatenates the value
 # of person[:first_name] and a message about them.
 # Write your response as a Ruby string.
 
-Diagnostic.hash_default = 'your answer here'
+Diagnostic.hash_default = 'Person.default["Hi, {#first_name} welcome to the Parks Department."]'
